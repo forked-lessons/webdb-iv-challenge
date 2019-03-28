21cbb6b962663bbbabc2d17c70d4c9a270bdaed2
Daniel <danielstarling4@gmail.com>
2019-03-28 13:03:07 -0700

    db migrations and seeds created for dishes

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ const express = require('express');
 const helmet = require('helmet');
 const knex = require('knex');
 
+const knexConfig = {
+  client: 'sqlite3',
+  connection: {
+    filename: './data/recipe_book.db3'
+  },
+  useNullAsDefault: true, // needed for sqlite
+  migrations: {
+    directory: './data/migrations'
+  },
+  seeds: {
+    directory: './data/seeds'
+  },
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run('PRAGMA foreign_keys = ON', done);
+    }
+  }
+};
+
+const db = knex(knexConfig);
+
 const server = express();
 
 server.use(helmet());
@@ -10,7 +31,15 @@ server.use(express.json());
 server.get('/', (req, res) => {
   res.status(201).json('API ONLINE');
 });
-
+server.get('/api/dishes', async (req, res) => {
+  // get the dishes from the database
+  try {
+    const dishes = await db('dishes'); // all the records from the table
+    res.status(200).json(dishes);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 const port = process.env.PORT || 5000;
 server.listen(port, () =>
   console.log(`\n** API running on http://localhost:${port} **\n`)
--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,10 @@
-// Update with your config settings.
+// // Update with your config settings.
 
 module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/recipebook.db3'
+      filename: './data/recipe_book.db3'
     },
     useNullAsDefault: true // needed for sqlite
   },
