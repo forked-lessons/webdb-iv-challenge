b95b47891aea0b0333813a9bf9b60eabfaee6c5a
Daniel <danielstarling4@gmail.com>
2019-03-28 12:23:11 -0700

    API online

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const express = require('express')
-const
+const express = require('express');
+const helmet = require('helmet');
+const knex = require('knex');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.status(201).json('API ONLINE');
+});
+
+const port = process.env.PORT || 5000;
+server.listen(port, () =>
+  console.log(`\n** API running on http://localhost:${port} **\n`)
+);
